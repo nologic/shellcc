7324fa4805e99d83a7f02eefc034293dd7e16d69
Mikhail Sosonkin <nologic@users.noreply.github.com>
2015-11-02 23:00:26 -0500

    untested support for x86_64

--- shellcode/syscalls.c
+++ shellcode/syscalls.c
@@ -1,150 +1,220 @@
 // to be included at the bottom of the shellcode main file.
 
+#ifdef __x86_64
+// set up parameters and calls for __x86_64
+
+#define REG_ARG1 "rdi"
+#define REG_SYSNUM "rax"
+#define REG_SYSRET "rax"
+
+#define DECL_ARG(name, reg)      register uint64_t name asm(reg)
+#define COPY_ARG(dest, src, reg) DECL_ARG(dest, reg) = src
+
+#define COPY_SYSNUM(dest, src) COPY_ARG(dest, src, REG_SYSNUM)
+#define COPY_ARG1(dest, src)   COPY_ARG(dest, src, "rdi")
+#define COPY_ARG2(dest, src)   COPY_ARG(dest, src, "rsi")
+#define COPY_ARG3(dest, src)   COPY_ARG(dest, src, "rdx")
+#define COPY_ARG4(dest, src)   COPY_ARG(dest, src, "r10")
+#define COPY_ARG5(dest, src)   COPY_ARG(dest, src, "r8")
+#define COPY_ARG6(dest, src)   COPY_ARG(dest, src, "r9")
+
+// These are not used by the kernel, we include them to get the 
+//   functions to compile.
+#define COPY_ARG7(dest, src)   COPY_ARG(dest, src, "rcx")
+#define COPY_ARG8(dest, src)   COPY_ARG(dest, src, "r11")
+
+#define SYS_CALL_ASM \
+		"syscall;\n"  \
+		".byte 0x73; .byte 0x03;\n" \
+		"negq %%rax;\n" 
+
+#elif __arm64
+// set up parameters and calls for __arm64
+
+#define REG_ARG1 "x0"
+#define REG_SYSNUM "x16"
+#define REG_SYSRET "x0"
+
+#define DECL_ARG(name, reg)      register uint64_t name asm(reg)
+#define COPY_ARG(dest, src, reg) DECL_ARG(dest, reg) = src
+
+#define COPY_SYSNUM(dest, src) COPY_ARG(dest, src, REG_SYSNUM)
+#define COPY_ARG1(dest, src)   COPY_ARG(dest, src, "x0")
+#define COPY_ARG2(dest, src)   COPY_ARG(dest, src, "x1")
+#define COPY_ARG3(dest, src)   COPY_ARG(dest, src, "x2")
+#define COPY_ARG4(dest, src)   COPY_ARG(dest, src, "x3")
+#define COPY_ARG5(dest, src)   COPY_ARG(dest, src, "x4")
+#define COPY_ARG6(dest, src)   COPY_ARG(dest, src, "x5")
+#define COPY_ARG7(dest, src)   COPY_ARG(dest, src, "x6")
+#define COPY_ARG8(dest, src)   COPY_ARG(dest, src, "x7")
+
 #define SYS_CALL_ASM \
 		"svc 0x80;\n" \
 		"neg x1, x0;\n" \
 		"csel x0, x0, x1, cc;\n" 
 
+#else
+	#error "Unsupported architecture"
+#endif
+
 static uint64_t scc_syscall0(uint64_t num) {
-	register uint64_t _num asm("x16") = num;
-	register uint64_t _arg1 asm("x0");
+	COPY_SYSNUM(_num, num);
+	DECL_ARG(_arg1, REG_ARG1);
+
+	DECL_ARG(_ret, REG_SYSRET);
+
 
 	__asm__(
 		SYS_CALL_ASM
-		:"=r"(_arg1)
+		:"=r"(_ret)
 		:"r"(_arg1), "r"(_num));
 
-	return _arg1;
+	return _ret;
 }
 
 static uint64_t scc_syscall1(uint64_t arg1, 
 						 uint64_t num) {
-	register uint64_t _num asm("x16") = num;
-	register uint64_t _arg1 asm("x0") = arg1;
+	COPY_SYSNUM(_num, num);
+	COPY_ARG1(_arg1, arg1);
+
+	DECL_ARG(_ret, REG_SYSRET);
 
 	__asm__(
 		SYS_CALL_ASM
-		:"=r"(_arg1)
+		:"=r"(_ret)
 		:"r"(_arg1), "r"(_num));
 
-	return _arg1;
+	return _ret;
 }
 
 static uint64_t scc_syscall2(uint64_t arg1, uint64_t arg2, uint64_t num) {
-	register uint64_t _num asm("x16") = num;
-	register uint64_t _arg1 asm("x0") = arg1;
-	register uint64_t _arg2 asm("x1") = arg2;
+	COPY_SYSNUM(_num, num);
+	COPY_ARG1(_arg1, arg1);
+	COPY_ARG2(_arg2, arg2);
+
+	DECL_ARG(_ret, REG_SYSRET);
 
 	__asm__(
 		SYS_CALL_ASM
-		:"=r"(_arg1)
-		:"r"(_arg1), "r"(_arg2),"r"(_num)
-		:"x0");
+		:"=r"(_ret)
+		:"r"(_arg1), "r"(_arg2),"r"(_num));
 
-	return arg1;
+	return _ret;
 }
 
 static uint64_t scc_syscall3(uint64_t arg1, uint64_t arg2, uint64_t arg3, 
 						 uint64_t num) {
-	register uint64_t _num asm("x16") = num;
-	register uint64_t _arg1 asm("x0") = arg1;
-	register uint64_t _arg2 asm("x1") = arg2;
-	register uint64_t _arg3 asm("x2") = arg3;
+	COPY_SYSNUM(_num, num);
+	COPY_ARG1(_arg1, arg1);
+	COPY_ARG2(_arg2, arg2);
+	COPY_ARG3(_arg3, arg3);
+
+	DECL_ARG(_ret, REG_SYSRET);
 
 	__asm__(
 		SYS_CALL_ASM
-		:"=r"(_arg1)
+		:"=r"(_ret)
 		:"r"(_arg1), "r"(_arg2),"r"(_arg3),"r"(_num));
 
-	return _arg1;
+	return _ret;
 }
 
 static uint64_t scc_syscall4(uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4, 
 						 uint64_t num) {
-	register uint64_t _num asm("x16") = num;
-	register uint64_t _arg1 asm("x0") = arg1;
-	register uint64_t _arg2 asm("x1") = arg2;
-	register uint64_t _arg3 asm("x2") = arg3;
-	register uint64_t _arg4 asm("x3") = arg4;
+	COPY_SYSNUM(_num, num);
+	COPY_ARG1(_arg1, arg1);
+	COPY_ARG2(_arg2, arg2);
+	COPY_ARG3(_arg3, arg3);
+	COPY_ARG4(_arg4, arg4);
+
+	DECL_ARG(_ret, REG_SYSRET);
 
 	__asm__(
 		SYS_CALL_ASM
-		:"=r"(_arg1)
+		:"=r"(_ret)
 		:"r"(_arg1), "r"(_arg2),"r"(_arg3),"r"(_arg4),"r"(_num));
 
-	return _arg1;
+	return _ret;
 }
 
 static uint64_t scc_syscall5(uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4,
 	                         uint64_t arg5, uint64_t num) {
-	register uint64_t _num asm("x16") = num;
-	register uint64_t _arg1 asm("x0") = arg1;
-	register uint64_t _arg2 asm("x1") = arg2;
-	register uint64_t _arg3 asm("x2") = arg3;
-	register uint64_t _arg4 asm("x3") = arg4;
-	register uint64_t _arg5 asm("x4") = arg5;
+	COPY_SYSNUM(_num, num);
+	COPY_ARG1(_arg1, arg1);
+	COPY_ARG2(_arg2, arg2);
+	COPY_ARG3(_arg3, arg3);
+	COPY_ARG4(_arg4, arg4);
+	COPY_ARG5(_arg5, arg5);
+
+	DECL_ARG(_ret, REG_SYSRET);
 
 	__asm__(
 		SYS_CALL_ASM
-		:"=r"(_arg1)
+		:"=r"(_ret)
 		:"r"(_arg1), "r"(_arg2),"r"(_arg3),"r"(_arg4),"r"(_arg5),"r"(_num));
 
-	return _arg1;
+	return _ret;
 }
 
 static uint64_t scc_syscall6(uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4,
 	                         uint64_t arg5, uint64_t arg6, uint64_t num) {
-	register uint64_t _num asm("x16") = num;
-	register uint64_t _arg1 asm("x0") = arg1;
-	register uint64_t _arg2 asm("x1") = arg2;
-	register uint64_t _arg3 asm("x2") = arg3;
-	register uint64_t _arg4 asm("x3") = arg4;
-	register uint64_t _arg5 asm("x4") = arg5;
-	register uint64_t _arg6 asm("x5") = arg6;
+	COPY_SYSNUM(_num, num);
+	COPY_ARG1(_arg1, arg1);
+	COPY_ARG2(_arg2, arg2);
+	COPY_ARG3(_arg3, arg3);
+	COPY_ARG4(_arg4, arg4);
+	COPY_ARG5(_arg5, arg5);
+	COPY_ARG6(_arg6, arg6);
+
+	DECL_ARG(_ret, REG_SYSRET);
 
 	__asm__(
 		SYS_CALL_ASM
-		:"=r"(_arg1)
+		:"=r"(_ret)
 		:"r"(_arg1), "r"(_arg2),"r"(_arg3),"r"(_arg4),"r"(_arg5),"r"(_arg6),"r"(_num));
 
-	return _arg1;
+	return _ret;
 }
 
 static uint64_t scc_syscall7(uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4,
 	                         uint64_t arg5, uint64_t arg6, uint64_t arg7, uint64_t num) {
-	register uint64_t _num asm("x16") = num;
-	register uint64_t _arg1 asm("x0") = arg1;
-	register uint64_t _arg2 asm("x1") = arg2;
-	register uint64_t _arg3 asm("x2") = arg3;
-	register uint64_t _arg4 asm("x3") = arg4;
-	register uint64_t _arg5 asm("x4") = arg5;
-	register uint64_t _arg6 asm("x5") = arg6;
-	register uint64_t _arg7 asm("x6") = arg7;
+	COPY_SYSNUM(_num, num);
+	COPY_ARG1(_arg1, arg1);
+	COPY_ARG2(_arg2, arg2);
+	COPY_ARG3(_arg3, arg3);
+	COPY_ARG4(_arg4, arg4);
+	COPY_ARG5(_arg5, arg5);
+	COPY_ARG6(_arg6, arg6);
+	COPY_ARG7(_arg7, arg7);
+
+	DECL_ARG(_ret, REG_SYSRET);
 
 	__asm__(
 		SYS_CALL_ASM
-		:"=r"(_arg1)
+		:"=r"(_ret)
 		:"r"(_arg1), "r"(_arg2),"r"(_arg3),"r"(_arg4),"r"(_arg5),"r"(_arg6),"r"(_arg7),"r"(_num));
 
-	return _arg1;
+	return _ret;
 }
 
 static uint64_t scc_syscall8(uint64_t arg1, uint64_t arg2, uint64_t arg3, uint64_t arg4,
 	                         uint64_t arg5, uint64_t arg6, uint64_t arg7, uint64_t arg8,uint64_t num) {
-	register uint64_t _num asm("x16") = num;
-	register uint64_t _arg1 asm("x0") = arg1;
-	register uint64_t _arg2 asm("x1") = arg2;
-	register uint64_t _arg3 asm("x2") = arg3;
-	register uint64_t _arg4 asm("x3") = arg4;
-	register uint64_t _arg5 asm("x4") = arg5;
-	register uint64_t _arg6 asm("x5") = arg6;
-	register uint64_t _arg7 asm("x6") = arg7;
-	register uint64_t _arg8 asm("x7") = arg8;
+	COPY_SYSNUM(_num, num);
+	COPY_ARG1(_arg1, arg1);
+	COPY_ARG2(_arg2, arg2);
+	COPY_ARG3(_arg3, arg3);
+	COPY_ARG4(_arg4, arg4);
+	COPY_ARG5(_arg5, arg5);
+	COPY_ARG6(_arg6, arg6);
+	COPY_ARG7(_arg7, arg7);
+	COPY_ARG8(_arg8, arg8);
+
+	DECL_ARG(_ret, REG_SYSRET);
 
 	__asm__(
 		SYS_CALL_ASM
-		:"=r"(_arg1)
+		:"=r"(_ret)
 		:"r"(_arg1), "r"(_arg2),"r"(_arg3),"r"(_arg4),"r"(_arg5),"r"(_arg6),"r"(_arg7),"r"(_arg8),"r"(_num));
 
-	return _arg1;
+	return _ret;
 }
