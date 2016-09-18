0463427358df375628715d3d750ba42ca69086f5
Mikhail Sosonkin <mikhail@synack.com>
2016-09-18 10:57:50 -0400

    publishing challenge solution

--- inject_dylib/inject.c
+++ inject_dylib/inject.c
@@ -1,3 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#if !defined(_DYLD_INTERPOSING_H_)
+#define _DYLD_INTERPOSING_H_
+
+/*
+ *  Example:
+ *
+ *  static
+ *  int
+ *  my_open(const char* path, int flags, mode_t mode)
+ *  {
+ *    int value;
+ *    // do stuff before open (including changing the arguments)
+ *    value = open(path, flags, mode);
+ *    // do stuff after open (including changing the return value(s))
+ *    return value;
+ *  }
+ *  DYLD_INTERPOSE(my_open, open)
+ */
+
+#define DYLD_INTERPOSE(_replacement,_replacee) \
+   __attribute__((used)) static struct{ const void* replacement; const void* replacee; } _interpose_##_replacee \
+            __attribute__ ((section ("__DATA,__interpose"))) = { (const void*)(unsigned long)&_replacement, (const void*)(unsigned long)&_replacee };
+
+#endif
+
 int foo() {
 	return 42;
 }
+
+int my_exit(int code) {
+	printf("I AM EXITING %d\n", code);
+	exit(code);
+}
+
+DYLD_INTERPOSE(my_exit, exit);
